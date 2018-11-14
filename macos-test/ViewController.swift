04eff47afbf9f2bb6ea727d9e398eb04ffc9e457
Daniel Kim <t-daki@microsoft.com>
2018-11-13 16:38:37 -0800

    first commit

---
+++ macos-test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macos-test
+//
+//  Created by Daniel Kim on 11/13/18.
+//  Copyright © 2018 Daniel Kim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
